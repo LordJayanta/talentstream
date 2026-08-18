7f211dba423db2c1286ae620b8c8b89a5c5f749d
LordJayanta <techie.jayanta@gmail.com>
2026-08-18 19:25:15 +0530

    feat: connect drizzle with postgres

---
+++ src/db/index.ts
@@ -0,0 +1,6 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+
+
+const connectionString = process.env.DATABASE_URL!;
+
+export const db = drizzle(connectionString);
---
+++ src/db/schema.ts
@@ -0,0 +1,21 @@
+import { pgTable, text,integer, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const jobsTable = pgTable("jobs", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  title: text("title").notNull(),
+  experience: text("experience"),
+  department: text("department"),
+  employmentType: text("employment_type"),
+  location: text("location"),
+  description: text("description").notNull(), // Stores your markdown text from the editor
+  minSalary: integer("min_salary"),
+  maxSalary: integer("max_salary"),
+  companyBenefits: text("company_benefits").array(),
+  mustHaveSkills: text("must_have_skills").array(),
+  niceToHaveSkills: text("Nice_to_have_skills").array(),
+  status: text("status").default("active").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export type InsertJob = typeof jobsTable.$inferInsert;
+export type SelectJob = typeof jobsTable.$inferSelect;
